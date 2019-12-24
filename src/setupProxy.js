46d4a26cdfb4e4a049b70699193dd2f7644eb234
Vitaliy Vanyushin <vitalij.vanyushin@simbirsoft.com>
2019-12-24 16:15:19 +0400

    setup proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,13 @@
+/** Note: This file only supports Node's JavaScript syntax. Be sure to only use supported language features (i.e. no support for Flow, ES Modules, etc). */
+const proxy = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    proxy('/api/v1/' /** '' */, {
+      target: 'https://me-dev.docker.simbirsoft',
+      changeOrigin: true,
+      secure: false,
+      logLevel: 'debug'
+    })
+  );
+};
